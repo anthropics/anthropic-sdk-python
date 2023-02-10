212a3699dbfdd8b7dfb5ff39ba7e5639f1d44ec1
Jackson Kernion <jackson@anthropic.com>
2023-02-10 11:29:25 -0800

    remove extra \n, use claude-v1

--- examples/basic_sync.py
+++ examples/basic_sync.py
@@ -3,11 +3,11 @@ import anthropic
 
 
 def main(max_tokens_to_sample: int = 100):
-    c = anthropic.Client(os.environ['ANTHROPIC_API_KEY'])
+    c = anthropic.Client(os.environ["ANTHROPIC_API_KEY"])
     resp = c.completion(
         prompt=f"{anthropic.HUMAN_PROMPT} How many toes do dogs have?{anthropic.AI_PROMPT}",
-        stop_sequences = [anthropic.HUMAN_PROMPT],
-        model="claude-v0",
+        stop_sequences=[anthropic.HUMAN_PROMPT],
+        model="claude-v1",
         max_tokens_to_sample=max_tokens_to_sample,
     )
     print(resp)
