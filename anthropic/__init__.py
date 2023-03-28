f544ccba241f7e98176017602b174d77e9aa38ed
Mike Lambert <mike.lambert@anthropic.com>
2023-03-28 16:34:42 -0700

    Add some length checks on the prompts and max-samples, to ensure they're within our acceptable range (#9)

--- anthropic/__init__.py
+++ anthropic/__init__.py
@@ -1,3 +1,3 @@
-from .api import Client
+from .api import ApiException, Client
 from .constants import HUMAN_PROMPT, AI_PROMPT, ANTHROPIC_CLIENT_VERSION
 from .tokenizer import count_tokens, get_tokenizer
