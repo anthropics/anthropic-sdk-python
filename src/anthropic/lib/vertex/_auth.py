20362be161401da11af38b26364743a8dd2e0b6e
John HU <hszqqq12@gmail.com>
2024-04-18 19:29:51 -0700

    Remove checks for project_id in load_auth()

--- src/anthropic/lib/vertex/_auth.py
+++ src/anthropic/lib/vertex/_auth.py
@@ -23,12 +23,6 @@ def load_auth() -> tuple[Credentials, str]:
     )
     credentials.refresh(Request())
 
-    if not project_id:
-        raise ValueError("Could not resolve project_id")
-
-    if not isinstance(project_id, str):
-        raise TypeError(f"Expected project_id to be a str but got {type(project_id)}")
-
     return credentials, project_id
 
 
