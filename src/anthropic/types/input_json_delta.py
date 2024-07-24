25a5b6c81ffb5996ef697aab22a22d8be5751bc1
Young-Jin Park <youngjinpark20@gmail.com>
2024-07-24 16:20:54 -0400

    feat: add back compat alias for InputJsonDelta

--- src/anthropic/types/input_json_delta.py
+++ src/anthropic/types/input_json_delta.py
@@ -4,10 +4,12 @@ from typing_extensions import Literal
 
 from .._models import BaseModel
 
-__all__ = ["InputJSONDelta"]
-
+__all__ = ["InputJSONDelta", "InputJsonDelta"]
 
 class InputJSONDelta(BaseModel):
     partial_json: str
 
     type: Literal["input_json_delta"]
+
+InputJsonDelta = InputJSONDelta
+
