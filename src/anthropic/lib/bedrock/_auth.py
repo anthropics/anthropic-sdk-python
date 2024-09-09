6ea5fce3b3a4d1ef4d5d3bbce8e27ea11e6dae72
Khalil Adib <121865320+khalilfiremind@users.noreply.github.com>
2024-09-09 11:41:32 +0100

    feat(bedrock): add `profile` argument to client (#648)

--- src/anthropic/lib/bedrock/_auth.py
+++ src/anthropic/lib/bedrock/_auth.py
@@ -17,10 +17,12 @@ def _get_session(
     aws_secret_key: str | None,
     aws_session_token: str | None,
     region: str | None,
+    profile: str | None,
 ) -> boto3.Session:
     import boto3
 
     return boto3.Session(
+        profile_name=profile,
         region_name=region,
         aws_access_key_id=aws_access_key,
         aws_secret_access_key=aws_secret_key,
@@ -37,12 +39,14 @@ def get_auth_headers(
     aws_secret_key: str | None,
     aws_session_token: str | None,
     region: str | None,
+    profile: str | None,
     data: str | None,
 ) -> dict[str, str]:
     from botocore.auth import SigV4Auth
     from botocore.awsrequest import AWSRequest
 
     session = _get_session(
+        profile=profile,
         region=region,
         aws_access_key=aws_access_key,
         aws_secret_key=aws_secret_key,
